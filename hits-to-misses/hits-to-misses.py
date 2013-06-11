8f09b31ab0c69e6181c9e260cde9e351b7ec469a
Prof. Scott F. H. Kaplan <sfkaplan@cs.amherst.edu>
2013-06-11 16:22:42 -0400

    Work in progress.

--- hits-to-misses/hits-to-misses.py
+++ hits-to-misses/hits-to-misses.py
@@ -14,8 +14,56 @@ import sys
 # The entry point.
 
 def main (argv):
-    for arg in argv:
-        print(arg);
+
+    # Check usage.
+    if len(argv) != 1:
+        print("USAGE: {0} <NO ARGUMENTS>\n".format(argv[0]));
+        print("       stdin: space-delimited, decimal text, hit histogram\n");
+        print("       stdout: space-delimited, decimal text, miss histogram\n");
+        exit(1)
+
+    # Read the hit histogram.
+    histogram = read_hits()
+
+    # Sum, right to left, to create the miss histogram.
+    sum(histogram)
+
+    # Emit the newly summed miss histogram.
+    write_misses(histogram)
+# ==================================================================================================================================
+
+
+
+# ==================================================================================================================================
+def read_hits ():
+
+    # Initialize the histogram.
+    histogram = [0]
+
+    # Read from stdin until there is no more.
+    for line in sys.stdin:
+
+        # Parse the current entry.
+        parsed_line = line.split()
+        qpos = int(parsed_line[0])
+        hits = int(parsed_line[1])
+
+        # Add it to the histogram list.
+        add_histogram_entry(histogram, qpos, hits)
+
+    return histogram
+# ==================================================================================================================================
+
+
+
+# ==================================================================================================================================
+
+# ==================================================================================================================================
+
+
+
+# ==================================================================================================================================
+
 # ==================================================================================================================================
 
 
@@ -24,5 +72,5 @@ def main (argv):
 # Trigger to call main() when invoked from the command line, passing the command-line arguments.
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+   main(sys.argv)
 # ==================================================================================================================================
