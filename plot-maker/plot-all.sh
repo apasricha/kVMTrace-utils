bf4eb40b705abce502b9b28023507463bc83efd3
Shruthi Badri 16 <sbadri16@csc2-node01.amherst.edu>
2013-06-26 15:08:24 -0400

    Changed script to clean up after itself

--- plot-maker/plot-all.sh
+++ plot-maker/plot-all.sh
@@ -12,7 +12,7 @@ foreach x ($l)
 	mv costdelay${x}.misses.pdf ${HOME}/kVMTrace-plots/costdelay_${x}.pdf
 	mv costperf${x}.misses.pdf ${HOME}/kVMTrace-plots/cost_perf_${x}.pdf
 	mv costppd${x}.misses.pdf ${HOME}/kVMTrace-plots/cost_ppd_${x}.pdf
-	rm -f *RAM:*
+	rm -f *RAM:* *.gnuplot
 	printf "done.\n"
 
 end
