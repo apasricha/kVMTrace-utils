2d58d7f5e8e8dcf6b45d819e574b83026d2ef4e8
tcorreia16 <tcorreia16@amherst.edu>
2013-08-21 13:08:25 -0400

    Added comparisons between original traces and traces through FIFO_Filter

--- trace-parser.c
+++ trace-parser.c
@@ -6,13 +6,14 @@
 
 int main (int argc, char *argv[])
 {
-  int o_bool, a_bool;
-  float quotient, *misses;
+  int o_bool, a_bool, ignore1, ignore2, line = 0;
+  float quotient;
+  long *misses;
   char *original_trace_filename, *altered_trace_filename;
   FILE *original_trace;
   FILE *altered_trace;
 
-  misses = malloc(2 * sizeof(float));
+  misses = malloc(2 * sizeof(long));
 
   original_trace_filename = argv[1];
   altered_trace_filename = argv[2];
@@ -20,18 +21,20 @@ int main (int argc, char *argv[])
   original_trace = fopen(original_trace_filename, "r");
   altered_trace = fopen(altered_trace_filename, "r");
 
-  o_bool = fscanf(original_trace, "%f", &misses[0]);
-  a_bool = fscanf(altered_trace, "%f", &misses[1]);
+  o_bool = fscanf(original_trace, "%d%ld", &ignore1,&misses[0]);
+  a_bool = fscanf(altered_trace, "%d%ld", &ignore2,&misses[1]);
 
-  while (o_bool != '\0' && a_bool != '0') {
+  while (o_bool == 2 && a_bool == 2) {
 
-    quotient = misses[1]/misses[0];
-    printf("%f\n", quotient);
+    quotient = ((float)misses[1]-misses[0])/misses[0];
+    //quotient = ((float)misses[1])/misses[0];
+    printf("%f  %d\n", quotient,line++);
 
-    o_bool = fscanf(original_trace, "%f", &misses[0]);
-    a_bool = fscanf(altered_trace, "%f", &misses[1]);
+    o_bool = fscanf(original_trace, "%d%ld", &ignore1, &misses[0]);
+    a_bool = fscanf(altered_trace, "%d%ld", &ignore2, &misses[1]);
   }
 
   free(misses);
+
 
 }
